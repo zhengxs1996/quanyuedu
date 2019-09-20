107399585212effecf5bdb8a67afa67a98657223
zhengxs <123444@aliyun.com>
2019-09-20 19:43:33 +0800

    添加text/java

---
+++ common/src/test/java/Demo.java
@@ -0,0 +1,8 @@
+/**
+ * @ClassName: Demo
+ * @Description: TODO
+ * @author: zhengxs
+ * @date: 2019/9/20  19:43
+ */
+public class Demo {
+}
---
+++ entity/src/test/java/Demo.java
@@ -0,0 +1,8 @@
+/**
+ * @ClassName: Demo
+ * @Description: TODO
+ * @author: zhengxs
+ * @date: 2019/9/20  19:42
+ */
+public class Demo {
+}
